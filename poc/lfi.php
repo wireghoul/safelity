8853e571f6e507d57e02093c11b1effe6eb926b5
Wireghoul <wireghoul@justanotherhacker.com>
2020-09-12 21:55:52 +1000

    Removed source exposure from poc files as library is now public

--- poc/lfi.php
+++ poc/lfi.php
@@ -3,20 +3,4 @@ require_once('../Safelity.php');
 
 require_safely("./",$_GET['fn']);
 
-/*
-function include_safely($dir, $file) {
-    $err = "No streams allowed";
-    if (no_stream($dir)) {
-        $err = "No such directory: $dir";
-        if (is_dir($dir)) {
-            $err = "Unable to include file: $file";
-            if (basename($file) === $file) {
-                return include($dir . $file);
-            }
-        }
-    }
-
-    error_safely($err);
-}
-*/
 ?>
--- poc/ping.php
+++ poc/ping.php
@@ -4,14 +4,4 @@ require_once('../Safelity.php');
 
 print exec_safely("ping",Array('-c 1', '--', $_GET['h']));
 
-/*
-function exec_safely($cmd, $args) {
-    $cmd = escapeshellcmd("$cmd ");
-    foreach ($args as $arg) {
-        $cmd.=escapeshellarg($arg )." ";
-    }
-    return shell_exec($cmd);
-}
-*/
-
 ?></pre>
