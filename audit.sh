d77cf8b0c9980a5379ffdc785ec24099c0466bfe
Wireghoul <wireghoul@justanotherhacker.com>
2020-09-29 16:07:05 +1000

    More auditing

--- audit.sh
+++ audit.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-grep --exclude=Safelity.php -rE '(file_get_contents|(include|require)(_once)?|exec|fopen|unlink)[[:space:]]*\(.*\$' $1
+echo
+echo "[*] Looking for use of unsafe functions"
+grep --colour=always --exclude=Safelity.php -rE '(file_get_contents|(include|require)(_once)?|exec|fopen|unlink)[[:space:]]*\(.*\$' $1
+echo
+echo "[*] Looking for unsafe invocation of Safelity file operations"
+grep --colour=always --exclude=Safelity.php -rE '(file_get_contents|(include|require)(_once)?|fopen|unlink)[[:space:]]*\(.*\$.*\,.*\$.*' $1
+echo
+echo "[*] Looking for exec_safely without argument termination (--)"
+grep  --colour=always --exclude=Safelity.php -r exec_safely $1 | grep -v -- '--'
