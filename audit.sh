988ace3529d0e9483fd3ba1fa7fc38ac89b852c9
Wireghoul <wireghoul@justanotherhacker.com>
2020-09-29 16:11:05 +1000

    Fixed regex

--- audit.sh
+++ audit.sh
@@ -4,7 +4,7 @@ echo "[*] Looking for use of unsafe functions"
 grep --colour=always --exclude=Safelity.php -rE '(file_get_contents|(include|require)(_once)?|exec|fopen|unlink)[[:space:]]*\(.*\$' $1
 echo
 echo "[*] Looking for unsafe invocation of Safelity file operations"
-grep --colour=always --exclude=Safelity.php -rE '(file_get_contents|(include|require)(_once)?|fopen|unlink)[[:space:]]*\(.*\$.*\,.*\$.*' $1
+grep --colour=always --exclude=Safelity.php -rE '(file_get_contents|include|fopen|unlink)_safely[[:space:]]*\(.*\$.*\,.*\$.*' $1
 echo
 echo "[*] Looking for exec_safely without argument termination (--)"
 grep  --colour=always --exclude=Safelity.php -r exec_safely $1 | grep -v -- '--'
