94ce1c96d164d74602f02a9c3ca3e8397e4d28f4
Wireghoul <wireghoul@justanotherhacker.com>
2020-09-29 16:12:08 +1000

    Added more non compliant examples

--- poc/noncompliant.php
+++ poc/noncompliant.php
@@ -1,4 +1,11 @@
 <?php
+include("../Safelity.php");
+
 $file="hello.txt";
 echo file_get_contents($file);
+
+$dir = "./";
+echo file_get_contents_safely($dir, $file);
+
+echo exec_safely("echo",array("1","2","3"));
 ?>
